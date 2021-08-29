636d4d132f8b841cda943577f861bd24054c1da8
AMJ <jacobsajibola@gmail.com>
2021-08-29 02:47:42 +0100

    Update db-update.sh
    Removed Errors

--- db-update/db-update.sh
+++ db-update/db-update.sh
@@ -22,7 +22,7 @@ done
 
 #Export remote database into a local file
 echo "Exporting remote DB"
-ssh $REMOTE_USER@$REMOTE_HOST "mysqldump --no-data  --defaults-file=.creds-remote.cfg --skip-lock-tables -h $REMOTE_DB_HOST -P $REMOTE_DB_PORT --protocol=tcp <schema> | sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' | 's/^INSERT/INSERT IGNORE/' > <schema> $IGNORED_TABLES_STRING $REMOTE_DB_SCHEMA" > $DUMP_FILE
+ssh $REMOTE_USER@$REMOTE_HOST "mysqldump --no-data  --defaults-file=.creds-remote.cfg --skip-lock-tables -h $REMOTE_DB_HOST -P $REMOTE_DB_PORT --protocol=tcp <schema> sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' 's/^INSERT/INSERT IGNORE/' <schema> $IGNORED_TABLES_STRING $REMOTE_DB_SCHEMA" > $DUMP_FILE
 
 #Remove remote db config file
 ssh $REMOTE_USER@$REMOTE_HOST  "rm ./.creds-remote.cfg"
